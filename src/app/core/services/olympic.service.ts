e379ac3340a5fb490ec141b5180328cda3aba081
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-11-26 06:33:36 +0100

    Refactor Olympics data model and service handling

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,23 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {Olympic} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympicUrl: string = './assets/mock/olympic.json';
+  private olympics$: BehaviorSubject<Olympic[] | null> = new BehaviorSubject<Olympic[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
+  loadInitialData(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      tap((value: Olympic[]): void => this.olympics$.next(value)),
+      catchError((error: HttpErrorResponse, caught: Observable<Olympic[]>):Observable<Olympic[]> => {
         // TODO: improve error handling
-        console.error(error);
+        console.error("Error loading data: ", error.message);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
         return caught;
@@ -25,7 +26,7 @@ export class OlympicService {
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<Olympic[] | null> {
     return this.olympics$.asObservable();
   }
 }
